45d664b784d318d77c5135880b63a7398eb3c519
ilove028 <maoxuheng@cnpc.com.cn>
2025-07-11 11:01:53 +0800

    chore: 结构化输出

--- src/ch-vision/index.js
+++ src/ch-vision/index.js
@@ -2,6 +2,7 @@ import OpenAI from "openai";
 import { readFileSync } from 'fs';
 import { getEnv } from "../util/index.js";
 import path from 'path';
+import * as z from 'zod';
 
 const openai = new OpenAI(
     {
@@ -15,8 +16,26 @@ const encodeImage = (imagePath) => {
     const imageFile = readFileSync(path.resolve(import.meta.dirname, '../../', imagePath));
     return imageFile.toString('base64');
   };
+
+const InvoiceSchema = z.object({
+    number: z.string().describe('发票号'),
+    date: z.string().describe('发票日期,格式为YYYY-MM-DD'),
+    price: z.object({
+        amount: z.number().describe('发票金额'),
+        currency: z.string().describe('发票金额的货币单位例如 CNY、USD、EUR、JPY等'),
+    }),
+    startStation: z.string().describe('出发站'),
+    endStation: z.string().describe('到达站'),
+    trainNumber: z.string().describe('火车车次'),
+    trainType: z.string().describe('火车类型'),
+    seatType: z.string().describe('座位类型'),
+    seatNumber: z.string().describe('座位号'),
+    passengerName: z.string().describe('乘客姓名'),
+    passengerIdNumber: z.string().describe('乘客身份证号'),
+    departureTime: z.string().describe('出发时间，格式为YYYY-MM-DD HH:mm'),
+});
 // 将xxx/eagle.png替换为你本地图像的绝对路径
-const base64Image = encodeImage("assets/images/math.png")
+const base64Image = encodeImage("assets/images/invoice.jpg")
 async function main() {
     const completion = await openai.chat.completions.create({
         model: "qwen-vl-max",  // 此处以qwen-vl-max-latest为例，可按需更换模型名称。模型列表：https://help.aliyun.com/model-studio/getting-started/model
@@ -29,8 +48,14 @@ async function main() {
                            // PNG图像：  data:image/png;base64,${base64Image}
                           // JPEG图像： data:image/jpeg;base64,${base64Image}
                          // WEBP图像： data:image/webp;base64,${base64Image}
-                        "image_url": {"url": `data:image/png;base64,${base64Image}`},},
-                        {"type": "text", "text": "分析图片里的问题给出详细解题步骤"}]}]
+                        "image_url": {"url": `data:image/jpeg;base64,${base64Image}`},},
+                        {"type": "text", "text": `提取图片中的信息，输出格式定义为${JSON.stringify(z.toJSONSchema(InvoiceSchema))}的JSON，不用输出JSON格式定义`}]}
+        ],
+        response_format: {
+            type: "json_object"
+        }
+
+
     });
     console.log(completion.choices[0].message.content);
 }
--- src/ch1/index.js
+++ src/ch1/index.js
@@ -1,8 +1,22 @@
 import OpenAI from "openai";
 import { getEnv } from "../util/index.js";
+import * as z from 'zod'
 
 async function main() {
   const config = getEnv()
+  const UserInfoSchema = z.object({
+    name: z.string().describe('客户姓名'),
+    company: z.string().describe('客户公司'),
+    id: z.string().describe('客户ID'),
+  })
+  const schema = z.object({
+    customerInfo: UserInfoSchema.describe('客户信息'),
+    version: z.string().describe('软件版本'),
+    errorCode: z.string().describe('错误代码'),
+    problemDesc: z.string().describe('问题描述'),
+    os: z.string().describe('操作系统'),
+    otherDetails: z.array(z.string()).nullable().describe('其他相关细节')
+  })
   const openai = new OpenAI(
       {
           // 若没有配置环境变量，请用百炼API Key将下行替换为：apiKey: "sk-xxx",
@@ -15,7 +29,9 @@ async function main() {
         model: "qwen-plus",  // qwen-plus 属于 qwen3 模型，如需开启思考模式，请参见：https://help.aliyun.com/zh/model-studio/deep-thinking
         messages: [
             { role: "system", content: "You are a helpful assistant." },
-            { role: "user", content: `请从这封客户反馈邮件中提取以下信息：1.客户信息 2.软件版本 3.错误代码 4.问题描述 5.操作系统 6.其他相关细节
+            { role: "user", content: `仔细阅读下面邮件，提取出Schema为${JSON.stringify(z.toJSONSchema(schema))}的json格式信息
+
+
 ------------
 邮件正文：
 主题：【报障求助】智能百炼助手启动故障
@@ -24,8 +40,8 @@ async function main() {
 自昨日下午起，我在启动智能百炼助手时遇到严重障碍，软件启动过程卡在初始化界面，错误码为ERR-2007，并弹出错误提示“数据库连接失败”，导致所有功能模块无法正常使用。这一情况已经持续至今，严重影响了我司的办公效率。
 我目前使用的服务器操作系统是Windows 10版本1909，针对64位架构进行优化。在遇到问题后，我已经采取了若干初步故障排除措施，其中包括完全关闭并重新启动系统以试图清除可能的临时软件冲突或系统挂起状态，以及彻底卸载并重新安装相关软件，旨在消除可能存在的软件损坏或配置错误问题。然而，尽管进行了这些常规解决尝试，问题仍然持续存在，未见任何改善。
 为了便于更深入地诊断问题，我已将详细的错误截图和系统日志文件作为附件一并提供。这些材料应当能精确展示错误发生时的软件状态、异常详情以及任何相关的错误代码，为快速定位并解决当前遇到的技术障碍提供了关键信息。
-期待您的尽快回复！
-结果只返回json` }
+期待您的尽快回复！` }
+
         ],
         response_format: {
             type: "json_object"
